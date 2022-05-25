7caaef053e2a52958cec05545cebb31e7e3369d2
dhichii <adhicittamasran@gmail.com>
2022-05-25 23:07:59 +0800

    Create updateBookByIdHandler

--- src/handler.js
+++ src/handler.js
@@ -120,8 +120,64 @@ const getBookByIdHandler = (request, h) => {
   }).code(200);
 };
 
+const updateBookByIdHandler = (request, h) => {
+  const {bookId} = request.params;
+  const {
+    name,
+    year,
+    author,
+    summary,
+    publisher,
+    pageCount,
+    readPage,
+    reading,
+  } = request.payload;
+  const updatedAt = new Date().toISOString();
+
+  if (name === '' || name === undefined) {
+    return h.response({
+      status: 'fail',
+      message: 'Gagal memperbarui buku. Mohon isi nama buku',
+    }).code(400);
+  }
+
+  if (readPage > pageCount) {
+    return h.response({
+      status: 'fail',
+      message: 'Gagal memperbarui buku. readPage tidak boleh lebih besar dari pageCount',
+    }).code(400);
+  }
+
+  const index = books.findIndex((book) => book.id === bookId);
+  if (index < 0) {
+    return h.response({
+      status: 'fail',
+      message: 'Gagal memperbarui buku. Id tidak ditemukan',
+    }).code(404);
+  }
+
+  books[index] = {
+    ...books[index],
+    name,
+    year,
+    author,
+    summary,
+    publisher,
+    pageCount,
+    readPage,
+    reading,
+    updatedAt,
+  };
+
+  return h.response({
+    status: 'success',
+    message: 'Buku berhasil diperbarui',
+  }).code(200);
+};
+
 module.exports = {
   addBookHandler,
   getAllBooksHandler,
   getBookByIdHandler,
+  updateBookByIdHandler,
 };
